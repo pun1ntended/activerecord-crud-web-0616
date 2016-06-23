395ef1d04356ec2b57db2d767fcfd56d09051775
Steven McGrath <mcgrath.steven7@gmail.com>
2016-06-23 10:39:00 -0400

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -19,78 +19,86 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.create(attributes)
+
 end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    __
+    m.title = "Home Alone"
+    m.release_date = 1990
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last.title
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1).title
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000, 
   # director == "Me"
-  __
+  movie = Movie.find_by_sql "SELECT * FROM movies WHERE title = 'Title' AND release_date = '2000' AND director = 'Me'"
+  movie[0]
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  Movie.order(release_date: :desc).where('release_date > ?', 2002)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
-  Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  identifier = Movie.find_by_sql("SELECT id FROM movies WHERE title = 'Awesome Flick'")
+  Movie.update(identifier[0].id, :title => "Even Awesomer Flick")
+  movie = Movie.find(identifier[0].id)
+  movie.save
+  
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  identifier = Movie.find_by_sql "SELECT id FROM movies WHERE title = 'Wat?'"
+  Movie.update(identifier[0].id, :title => 'Wat, huh?')
+  movie = Movie.find(identifier[0].id)
+  movie.save
 end
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
+    identifier = Movie.find_by_sql "SELECT id FROM movies WHERE title = 'Movie_#{i}'"
+    Movie.update(identifier[0].id, :title => "A Movie")
   end
-  __
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  identifier = Movie.find_by_sql "SELECT id FROM movies WHERE title = 'That One Where the Guy Kicks Another Guy Once'"
+  Movie.destroy(identifier[0].id)
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
+    identifier = Movie.find_by_sql "SELECT id FROM movies WHERE title = 'Movie_#{i}'"
+    Movie.destroy(identifier[0].id)
   end
-  __
 end
 
 
